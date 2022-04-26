0ccbab10010bb6165d71160d694707e74bb558fb
Eduardo Diniz <dinizdudi@gmail.com>
2022-04-26 20:48:26 -0300

    initial commit

---
+++ src/test/java/br/com/curso/aws/project01/Project01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.aws.project01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
